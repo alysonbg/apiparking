fcd285051f0e7becc77c2718bab7398bdca056ac
Alyson Girao <alysongirao@gmail.com>
2019-11-07 22:10:43 -0300

    Adicionado Parking model

--- parkinglot/core/models.py
+++ parkinglot/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Parking(models.Model):
+    plate = models.CharField(max=8, required=True)
+    time = models.TimeField()
+    paid = models.BooleanField()
+    left = models.BooleanField()
