726473fd62188fcab9d29e9fcf4e804b6b2f64b7
Alyson Girao <alysongirao@gmail.com>
2019-11-10 16:41:36 -0300

    Adicionado metodo validate_plate para validar a mascara do campo plate

--- parkinglot/core/serializers.py
+++ parkinglot/core/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from .models import Parking
 
@@ -11,6 +12,16 @@ class ParkingSerializer(serializers.ModelSerializer):
 class CreateParkingSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     plate = serializers.CharField(write_only=True)
+
+    def validate_plate(self, plate):
+        regex_validacao = re.compile('[A-Z]{3}-[0-9]{4}')
+
+        if  not regex_validacao.match(plate):
+            raise serializers.ValidationError(
+               'A placa fornecida nao esta no formato AAA-9999'
+            )
+        return plate
+
     class Meta:
         model = Parking
         fields = ('id', 'plate')
--- parkinglot/core/tests.py
+++ parkinglot/core/tests.py
@@ -1,8 +1,10 @@
 from rest_framework.test import APITestCase
-
+from rest_framework import serializers
 
 class TestParking(APITestCase):
     def test_create_parking(self):
         """Testa se o parking é criado quando uma requisicao POST ao endpoing /parking"""
-        response = self.client.post('/parking/', {'plate': 'AA-9999'})
+        response = self.client.post('/parking/', {'plate': 'AAA-9999'})
         self.assertEqual({'id': 1}, response.data)
+
+        
